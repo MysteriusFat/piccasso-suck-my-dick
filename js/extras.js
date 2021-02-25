8da5c4fed0e2cad30a2ab4670610c06ab33cd6ff
Jose Ramirez <frijolito@ubuntu20.04>
2021-02-25 02:33:05 -0300

    update

--- js/extras.js
+++ js/extras.js
@@ -1,7 +1,7 @@
 populationSlide = document.getElementById("population");
 mutationRatioSlider = document.getElementById("mutation-ratio");
 dnaSizeSlider = document.getElementById("dna-size");
-
+inputFile = document.getElementById("file");
 
 populationSlide.addEventListener("change", function (event) {
     event.path[1].children[0].children[1].innerHTML = event.target.value;
@@ -17,4 +17,13 @@ dnaSizeSlider.addEventListener("change", function (event) {
     event.path[1].children[0].children[1].innerHTML = event.target.value;
 });
 
-
+file.addEventListener("change", function(event) {
+    var output = document.getElementById('upload-images');
+    output.src = URL.createObjectURL(event.target.files[0]);
+    output.onload = function() {
+        URL.revokeObjectURL(output.src) // free memory
+        const ctx = document.getElementById('canvasImg').getContext('2d');
+        ctx.clearRect(0, 0, 300, 450);
+        ctx.drawImage(output, 0, 0, width, height);
+    }
+});
